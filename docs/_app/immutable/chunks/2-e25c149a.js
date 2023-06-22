b9e7a4c1a4e34a82146d52cab01e4b3a2fd2f992
Simon Green <simon@captaincodeman.com>
2023-06-22 09:25:26 -0600

    update dependencies
    use github actions to deploy

--- docs/_app/immutable/chunks/2-e25c149a.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-dca33500.js";export{t as component};
--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/kit/vite'
 
-const prod = process.env.NODE_ENV === 'production'
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -10,14 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			pages: 'docs',
-			assets: 'docs',
-			fallback: null,
-		}),
-		paths: {
-			base: prod ? '/svelte-intersection-observer-action' : '',
-		},
+		adapter: adapter(),
 	},
 }
 
